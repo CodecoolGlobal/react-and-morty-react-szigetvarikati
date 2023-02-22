acf8590fa27357584ceec3142a4d1ed0a79b4903
Tomi1325 <113603371+Tomi1325@users.noreply.github.com>
2023-02-22 11:04:01 +0100

    fetch working

--- react-and-morty/src/components/characterPage.js
+++ react-and-morty/src/components/characterPage.js
@@ -1,23 +1,32 @@
-import { useState } from "react";
-import { useCharacters } from "../api/useData";
-import Card from "./card";
+import { useEffect, useState } from "react";
 
 function CharacterPage() {
-  const characters = useCharacters(1);
-  
- 
-  if (characters === null) {
-    return "Loading..."
-  }
-  console.log("Nemtommmiiiiii");
-  console.log(characters.results);
-  
+  const [characters, setCharacters] = useState([]);
+  useEffect(() => {
+    fetch('https://rickandmortyapi.com/api/character/?page=1')
+    .then((response) => response.json())
+    .then((data) => setCharacters(data));
+  }, [])
+
+  const showCharacter = Array.isArray(characters.results) && characters.results.map((character) => {
+    return( <div className="card">
+    <h2>{character.name}</h2>
+    <p>{character.species}</p>
+    <img src={character.image} alt='szia'></img>
+
+  </div>)
+
+  });
+
+  console.log(showCharacter)
   return (
     <div>
-
-      <Card />
+      <h1>Characters</h1>
+      <ul>
+        {showCharacter}
+      </ul>
     </div>
-  )
+  );
 }
 
 export default CharacterPage;
