7f277376c3244268f30d384ffcd8c0b5be0a3137
szigetvarikati <szigetvarikati@gmail.com>
2023-02-23 11:35:36 +0100

    add randomize pistures to the Locations

---
+++ react-and-morty/src/components/OneLocation.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function OneLocation({ character, onClose }) {
+  return (
+    <div className="oneChar">
+      <div className="oneCharContent">
+        <span className="close" onClick={onClose}>
+          &times;
+        </span>
+        <h2>Name: {character.name}</h2>
+        <h4>Type: {character.type}</h4>
+        <h4>Dimension: {character.dimension}</h4>
+        {/* <img className="charimg" src={character.image} alt={character.name} /> */}
+        <h6>ID: {character.id}</h6>
+      </div>
+    </div>
+  );
+}
+
+export default OneLocation;
--- react-and-morty/src/components/locationPage.js
+++ react-and-morty/src/components/locationPage.js
@@ -1,11 +1,31 @@
 import { useEffect, useState } from "react";
 import Button from "./button";
-import OneCharacter from "./OneCharacter";
+import pic1 from "./locations_pic/pic1.png";
+import pic2 from "./locations_pic/pic2.png";
+import pic3 from "./locations_pic/pic3.png";
+import pic4 from "./locations_pic/pic4.png";
+import pic5 from "./locations_pic/pic5.png";
+import pic6 from "./locations_pic/pic6.png";
+import pic7 from "./locations_pic/pic7.png";
+import pic8 from "./locations_pic/pic8.png";
+import pic9 from "./locations_pic/pic9.png";
+import pic10 from "./locations_pic/pic10.png";
+import pic11 from "./locations_pic/pic11.png";
+import pic12 from "./locations_pic/pic12.png";
+import pic13 from "./locations_pic/pic13.png";
+import OneLocation from "./OneLocation";
+
 
 function LocationPage({ displayNextPage, displayPrevPage }) {
   const [locations, setLocations] = useState([]);
   const [currentPage, setCurrentPage] = useState(1)
   const [selectedLocation, setSelectedLocation] = useState(null)
+  const locationPictures = [pic1, pic2, pic3, pic4, pic5, pic6, pic7, pic8, pic9, pic10, pic11, pic12, pic13];
+  
+  function getRandomPicture() {
+    const index = Math.floor(Math.random()*12 + 1);
+    return locationPictures[index]
+  }
 
   const url = "https://rickandmortyapi.com/api/location/?page="
   useEffect(() => {
@@ -20,6 +40,7 @@ function LocationPage({ displayNextPage, displayPrevPage }) {
       <div className="card" onClick={() => setSelectedLocation(location)}>
         <h2>{location.name}</h2>
         <p>{location.type}</p>
+        <img className="locPic" src={getRandomPicture()} alt="the location"></img>
       </div>
     )
   });
@@ -57,7 +78,7 @@ function LocationPage({ displayNextPage, displayPrevPage }) {
   return (
     <div>
       {selectedLocation && (
-        <OneCharacter character={selectedLocation} onClose={() => setSelectedLocation(null)} />
+        <OneLocation character={selectedLocation} onClose={() => setSelectedLocation(null)} />
       )}
       <div className="pagination">
         <Button text="Previous" onClick={displayPrevPage}/>
