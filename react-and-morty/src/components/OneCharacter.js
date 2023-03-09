76a0b04a6c37e52da266a2778717139ba1288710
Tomi1325 <113603371+Tomi1325@users.noreply.github.com>
2023-03-09 09:29:57 +0100

    status change works

--- react-and-morty/src/components/OneCharacter.js
+++ react-and-morty/src/components/OneCharacter.js
@@ -1,11 +1,17 @@
 import React from "react";
+import Button from "./button";
 
-function OneCharacter({ character, onClose }) {
+function OneCharacter({ character, onClose, handleKill }) {
+  const handleStatusChange = () => {
+    handleKill(character);
+    onClose();
+  };
+
   return (
     <div className="oneChar">
-        <span className="close" onClick={onClose}>
-          &times;
-        </span>
+      <span className="close" onClick={onClose}>
+        &times;
+      </span>
       <div className="oneCharContent">
         <h2>Name: {character.name}</h2>
         <h4>Species: {character.species}</h4>
@@ -13,9 +19,14 @@ function OneCharacter({ character, onClose }) {
         <h4>Gender: {character.gender}</h4>
         <img className="charimg" src={character.image} alt={character.name} />
         <h6>{character.id}</h6>
+        {character.status === "Alive" && (
+          <Button text="Kill" onClick={handleStatusChange} />
+        )}
+        {character.status === "Dead" && (
+          <Button text="Resurrect" onClick={handleStatusChange} />
+        )}
       </div>
     </div>
   );
 }
-
 export default OneCharacter;
--- react-and-morty/src/components/characterPage.js
+++ react-and-morty/src/components/characterPage.js
@@ -5,48 +5,90 @@ import Button from "./button";
 function CharacterPage() {
   const [characters, setCharacters] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const [selectedCharacter, setSelectedCharacter] = useState(null)
+  const [selectedCharacter, setSelectedCharacter] = useState(null);
 
-  useEffect(() => {
-    const url = '/api/characters'
+  const fetchCharacters = () => {
+    const url = "/api/characters";
     fetch(`${url}`)
       .then((response) => response.json())
       .then((data) => setCharacters(data));
-  }, [currentPage])
+  };
 
-  const showCharacter = Array.isArray(characters) && characters.map((character, index) => {
-    return (<div className="card" key={index} onClick={() => setSelectedCharacter(character)}>
-      <h2>{character.name}</h2>
-      <p>{character.species}</p>
-      <img className="charimg" src={character.image} alt={character.name}></img>
-      <h6>{character.id}</h6>
+  const handleKill = async (character) => {
+    const newStatus = character.status === "Alive" ? "Dead" : "Alive";
+    try {
+      const response = await fetch(`/api/characters/id/${character.id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ status: newStatus }),
+      });
+      const data = await response.json();
+      console.log(data);
+      fetchCharacters();
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    </div>)
-  });
+  useEffect(() => {
+    fetchCharacters();
+  }, [currentPage]);
+
+  const showCharacter =
+    Array.isArray(characters) &&
+    characters.map((character, index) => {
+      return (
+        <div
+          className="card"
+          key={index}
+          onClick={() => setSelectedCharacter(character)}
+        >
+          <h2>{character.name}</h2>
+          <p>{character.species}</p>
+          <img
+            className="charimg"
+            src={character.image}
+            alt={character.name}
+          ></img>
+          <h6>{character.id}</h6>
+        </div>
+      );
+    });
 
   function displayNextPage() {
-    if (currentPage < 42)
-      setCurrentPage(currentPage + 1)
+    if (currentPage < 42) setCurrentPage(currentPage + 1);
   }
   function displayPrevPage() {
     if (currentPage > 1) {
-      setCurrentPage(currentPage - 1)
+      setCurrentPage(currentPage - 1);
     }
   }
   return (
     <div>
       {selectedCharacter && (
-        <OneCharacter character={selectedCharacter} onClose={() => setSelectedCharacter(null)} />
+        <OneCharacter
+          character={selectedCharacter}
+          onClose={() => setSelectedCharacter(null)}
+          handleKill={handleKill}
+        />
       )}
       <div className="pagination">
-        <Button text='←' className='PageChangeButton' onClick={displayPrevPage} />
-        <Button text='→' className='PageChangeButton' onClick={displayNextPage} />
+        <Button
+          text="←"
+          className="PageChangeButton"
+          onClick={displayPrevPage}
+        />
+        <Button
+          text="→"
+          className="PageChangeButton"
+          onClick={displayNextPage}
+        />
       </div>
       <h1>Characters</h1>
       <p>{currentPage} of 42</p>
-      <div className="cardContainer">
-        {showCharacter}
-      </div>
+      <div className="cardContainer">{showCharacter}</div>
     </div>
   );
 }
